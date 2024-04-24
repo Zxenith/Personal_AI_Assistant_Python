fcec84ad17633911c800103ea5f9dab14f75121e
Akarshan Nirwan <141826161+Zxenith@users.noreply.github.com>
2024-04-24 23:24:23 +0530

    Update APA.py

--- APA.py
+++ APA.py
@@ -3,6 +3,8 @@ import datetime
 import pyttsx3 as pt3
 import speech_recognition as sr
 import datetime as dt
+import os
+import cv2
 
 #Engine initialisation.
 engine = pt3.init('sapi5')
@@ -26,14 +28,14 @@ def speak(audio):
 
 
 #Speech to text
-def commander(phrase_time_limit=5):
+def commander():
     recognizer = sr.Recognizer() #For recognition of speech detection
 
     with sr.Microphone() as source: #Considering microphone as the source
 
         recognizer.pause_threshold = 1 #For generating pauses between sentences
 
-        audio = recognizer.listen(source,timeout=1,phrase_time_limit=phrase_time_limit)
+        audio = recognizer.listen(source,timeout=1,phrase_time_limit=5)
 
     #Error handling must be further avoided.
 
@@ -52,7 +54,7 @@ def commander(phrase_time_limit=5):
 #Wishing command execution
 def wishme():
     speak("What is your name?")
-    name = commander(5)
+    name = commander()
     hour = int(dt.datetime.now().hour)
 
     if hour >= 4 and hour < 12:
@@ -68,4 +70,52 @@ def wishme():
 
 if __name__ == "__main__":
     # commander()
-    wishme()
+    # wishme()
+    while True:
+
+        query = commander()
+        query = query.lower()
+
+        #Taking tasks
+
+        if 'open notepad' in query:
+            path = "C:\\Windows\\System32\\notepad.exe"
+            speak("Opening Notepad")
+            os.startfile(path)
+            break
+
+
+        elif 'open counter strike' in query or 'open CS' in query:
+            path = "C:\\Users\\akars\\OneDrive\\Desktop\\Counter-Strike Global Offensive.url"
+            speak("Opening Counter Strike")
+            os.startfile(path)
+            break
+
+
+        elif 'open command prompt' in query:
+            speak('Opening Command Prompt')
+            os.system('start cmd')
+            break
+
+
+        elif 'open camera' in query:
+            cam = cv2.VideoCapture(0)
+
+            while True:
+                ret, img = cam.read()
+                cv2.imshow('webcam',img)
+                k = cv2.waitKey(50)
+                if cv2.getWindowProperty('webcam', cv2.WND_PROP_VISIBLE) < 1:
+                    break
+                if k == 27:
+                    break
+
+            cam.release()
+            cv2.destroyAllWindows()
+            break
+
+        elif 'play music' in query:
+            music = "C:\\404\\VirtualAssistant\\Music"
+            songs = os.listdir(music)
+            os.startfile(os.path.join(music,songs[0]))
+            break
